22e1ce9650279d357d3be7780f89abb895a2451d
marybratt <mbratt56@gmail.com>
2018-12-15 15:23:09 -0800

    changes to use random utility

--- guessMyNumber.java
+++ guessMyNumber.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class guessMyNumber {
 
 	public static void main(String[] args) {
 		// get a random integer to guess
-		int randomInt = getRandomNumber(100);
+		Random rand = new Random();
+		int randomNumber = rand.nextInt(100) + 1;
+		//int randomInt = getRandomNumber(100);
 		Scanner input = new Scanner(System.in);
 		System.out.println("Guess a number between 1 and 100");
 		int guess = input.nextInt();
@@ -12,7 +15,7 @@ public class guessMyNumber {
 		// message depending on if the numbers are equal or not
 		for(int i = 1; i<10;i++) {
 			//call checkIfEqual method
-			boolean isEqual = checkIfEqual(randomInt, guess);
+			boolean isEqual = checkIfEqual(randomNumber, guess);
 			//if not equal you will loop again, if equal will break out of loop
 			if(isEqual) {
 			//	System.out.println(randomInt);
@@ -24,7 +27,7 @@ public class guessMyNumber {
 		}
 		// once you have broken out of loop determine if the numbers were equal
 		// if not equal you would then let the user know that they have tried to many times
-		if (randomInt != guess) {
+		if (randomNumber != guess) {
 			System.out.println("To many tries!");
 		}
 		
@@ -34,22 +37,23 @@ public class guessMyNumber {
 		 * @param highestNumber will determine the highest number to use
 		 * @return random number in the range of 1 to sides	
 		 */
-	public static int getRandomNumber(int highestNumber) {
-		//random number between 0 and (almost 1)
-		double randomNumber = Math.random();				
-		//change range to 0 to almost the number of sides being sent in
-		randomNumber = randomNumber * highestNumber;
-				
-		// shift range up one
-		randomNumber += 1;
-				
-		//cast to integer (ignore decimal part)
-		// ex. 6.998 become 6
-		int randomInt = (int) randomNumber;
-				
-		// return statement
-		return randomInt;
-	}	
+//	public static int getRandomNumber(int highestNumber) {
+//		//random number between 0 and (almost 1)
+//		double randomNumber = Math.random();
+//	
+//		//change range to 0 to almost the number of sides being sent in
+//		randomNumber = randomNumber * highestNumber;
+//				
+//		// shift range up one
+//		randomNumber += 1;
+//				
+//		//cast to integer (ignore decimal part)
+//		// ex. 6.998 become 6
+//		int randomInt = (int) randomNumber;
+//				
+//		// return statement
+//		return randomInt;
+//	}	
 	
 	/**
 	 * Compares the random integer  that was created with the number entered by the user 
@@ -61,6 +65,7 @@ public class guessMyNumber {
 
 	public static boolean checkIfEqual(int randomInt, int guess) {
 		boolean isEqual = false;
+		System.out.println(randomInt);
 		if(randomInt == guess) {
 			isEqual = true;
 			System.out.println("Congratulations, you have guessed the correct number!");
@@ -81,4 +86,4 @@ public class guessMyNumber {
 
 
 
-# GuessMyNumber
+//# GuessMyNumber
